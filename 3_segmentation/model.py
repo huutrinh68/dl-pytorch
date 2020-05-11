5f41896cb3791532486ed6476cf7ed9978c5fa1a
huutrinh68 <trinhsp89@gmail.com>
2020-05-12 00:57:27 +0900

    add bottleNeckPSP, bottleNeckIdentifyPSP

--- 3_segmentation/model.py
+++ 3_segmentation/model.py
@@ -58,6 +58,54 @@ class ResidualBlockPSP(nn.Sequential):
             self.add_module("block" + str(i+2), bottleNeckIdentifyPSP(out_channels, mid_channels, stride, dilation))
 
 
+class conv2DBatchNorm(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, dilation, bias):
+        super(conv2DBatchNorm, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation, bias=bias)
+        self.batchnorm = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        x = self.conv(x)
+        outputs = self.batchnorm(x)
+
+        return outputs
+
+
+class bottleNeckPSP(nn.Module):
+    def __init__(self, in_channels, mid_channels, out_channels, stride, dilation):
+        super(bottleNeckPSP, self).__init__()
+
+        self.cbr_1 = conv2DBatchNormRelu(in_channels, mid_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False)
+        self.cbr_2 = conv2DBatchNormRelu(mid_channels, mid_channels, kernel_size=3, stride=stride, padding=dilation, dilation=dilation, bias=False)
+        self.cb_3 = conv2DBatchNorm(mid_channels, outputs, kernel_size=1, stride=1, padding=0, dilation=1, bias=False)
+
+        # skip connection
+        self.cb_residual = conv2DBatchNorm(in_channels, out_channels, kernel_size=1, stride=stride, padding=0, dilation=1, bias=False)
+        self.relu = nn.ReLU(inplace=True)
+
+
+    def forward(self, x):
+        conv = sefl.cb_3(self.cbr_2(self.cbr_1(x)))
+        residual = self.cb_residual(x)
+
+        return self.relu(conv + residual)
+
+
+class bottleNeckIdentifyPSP(nn.Module):
+    def __init__(self, in_channels, mid_channels, stride, dilation):
+        super(bottleNeckIdentifyPSP, self).__init__()
+
+        self.cbr_1 = conv2DBatchNormRelu(in_channels, mid_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False)
+        self.cbr_2 = conv2DBatchNormRelu(mid_channels, mid_channels, kernel_size=3, stride=1, padding=dilation, dilation=dilation, bias=False)
+        self.cb_3 = conv2DBatchNorm(mid_channels, in_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        conv = self.cb_3(sefl.cbr_2(self.cbr_1(x)))
+        residual = x
+
+        return self.relu(conv + residual)
+
 
 
 if __name__ == "__main__":
