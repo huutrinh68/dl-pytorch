d2284c392ad91bd78989aee4fc01bcb5e34de252
huutrinh68 <trinhsp89@gmail.com>
2020-05-22 00:25:33 +0900

    add pyramid pooling

--- 3_segmentation/model.py
+++ 3_segmentation/model.py
@@ -1,5 +1,6 @@
 import torch 
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class conv2DBatchNormRelu(nn.Module):
@@ -83,7 +84,6 @@ class bottleNeckPSP(nn.Module):
         self.cb_residual = conv2DBatchNorm(in_channels, out_channels, kernel_size=1, stride=stride, padding=0, dilation=1, bias=False)
         self.relu = nn.ReLU(inplace=True)
 
-
     def forward(self, x):
         conv = sefl.cb_3(self.cbr_2(self.cbr_1(x)))
         residual = self.cb_residual(x)
@@ -105,6 +105,45 @@ class bottleNeckIdentifyPSP(nn.Module):
         residual = x
 
         return self.relu(conv + residual)
+
+
+class PyramidPooling(nn.Module):
+    def __init__(self, in_channels, pool_sizes, height, width):
+        super(PyramidPooling, self).__init__()
+
+        self.height = height
+        self.width = width
+
+        out_channels = int(in_channels/len(pool_sizes))
+        # pool_sizes = [6,3,2,1]
+        self.avpool_1 = nn.AdaptiveAvgPool2d(output_size=pool_sizes[0])
+        self.cbr_1 = conv2DBatchNormRelu(in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False)
+
+        self.avpool_2 = nn.AdaptiveAvgPool2d(output_size=pool_sizes[1])
+        self.cbr_2 = conv2DBatchNormRelu(in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False)
+
+        self.avpool_3 = nn.AdaptiveAvgPool2d(output_size=pool_sizes[2])
+        self.cbr_3 = conv2DBatchNormRelu(in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False)
+
+        self.avpool_4 = nn.AdaptiveAvgPool2d(output_size=pool_sizes[3])
+        self.cbr_4 = conv2DBatchNormRelu(in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False)
+
+
+    def forward(self, x):
+        out1 = self.cbr_1(self.avpool_1(x))
+        out1 = F.interpolate(out1, size=(self.height, self.width), mode='bilinear', align_corners=True)
+
+        out2 = self.cbr_2(self.avpool_2(x))
+        out2 = F.interpolate(out2, size=(self.height, self.width), mode='bilinear', align_corners=True)
+
+        out3 = self.cbr_3(self.avpool_3(x))
+        out3 = F.interpolate(out3, size=(self.height, self.width), mode='bilinear', align_corners=True)
+
+        out4 = self.cbr_4(self.avpool_2(x))
+        out4 = F.interpolate(out4, size=(self.height, self.width), mode='bilinear', align_corners=True)
+
+        output = torch.cat([x, out1, out2, out3, out4], dim=1)
+        return output
 
 
 
