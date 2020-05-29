739f45d81bd5157cbfc52530d216fc18f12ee339
huutrinh68 <trinhsp89@gmail.com>
2020-05-29 14:02:21 +0900

    add decode, auxloss

--- 3_segmentation/model.py
+++ 3_segmentation/model.py
@@ -146,6 +146,47 @@ class PyramidPooling(nn.Module):
         return output
 
 
+class DecodePSPFeature(nn.Module):
+    def __init__(self, height, width, n_classes):
+        super(DecodePSPFeature, self).__init__()
+
+        # forward
+        self.height = height
+        self.width = width
+
+        self.cbr = conv2DBatchNormRelu(in_channels=4096, out_channels=512, kernel_size=3, stride=1, padding=1, dilation=1, bias=False)
+        self.dropout = nn.Dropout(p=0.1)
+        self.classification = nn.Conv2d(in_channels=512, out_channels=n_classes, kernel_size=1, stride=1, paddding=0)
+
+    def forward(self, x):
+        x = self.cbr(x)
+        x = self.dropout(x)
+        x = self.classification(x)
+        output = F.interpolate(x, size=(self.height, self.width), mode="bilinear", align_corners=True)
+
+        return output
+
+
+class AuxilirayPSPLayers(nn.Module):
+    def __init__(self, in_channels, height, width, n_classes):
+        super(AuxilirayPSPLayers, self).__init__()
+
+        # forward
+        self.height = height
+        self.width = width
+
+        self.cbr = conv2DBatchNormRelu(in_channels=in_channels, out_channels=256, kernel_size=3, stride=1, padding=1, dilation=1, bias=False)
+        self.dropout = nn.Dropout(p=0.1)
+        self.classification = nn.Conv2d(in_channels=256, out_channels=n_classes, kernel_size=1, stride=1, padding=0)
+
+        def forward(self, x):
+            x = self.cbr(x)
+            x = self.dropout(x)
+            x = self.classification(x)
+            output = F.interpolate(x, size=(self.height, self.width), mode="bilinear", align_corners=True)
+            
+            return  output
+
 
 if __name__ == "__main__":
     x = torch.randn(1, 3, 475, 475)
